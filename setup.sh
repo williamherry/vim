c0cb37b2fde0c882f5176c3000688df08a6db7ac
William Herry <WilliamHerryChina@Gmail.com>
2012-08-17 21:15:48 +0800

    alias vi to vim

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # setup vimrc
 ln -sv ~/.vim/vimrc ~/.vimrc
+echo "alias vi='vim'" >> ~/.bashrc
 
 # install plugin base on ~/.vimrc
 vim +BundleInstall +qa
